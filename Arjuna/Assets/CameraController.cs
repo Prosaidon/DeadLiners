1eb4d4cc26aa73ed4a5683bbc1163ed4fb133dce
Prosaidon <rizkydayat.mr.x@gmail.com>
2023-11-15 13:15:10 +0700

    15/11/2023
    asset

--- Arjuna/Assets/CameraController.cs
+++ Arjuna/Assets/CameraController.cs
@@ -2,29 +2,21 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    //Room camera
-    [SerializeField] private float speed;
-    private float currentPosX;
-    private Vector3 velocity = Vector3.zero;
+    [SerializeField]
+    private Transform target;
 
-    //Follow player
-    [SerializeField] private Transform player;
-    [SerializeField] private float aheadDistance;
-    [SerializeField] private float cameraSpeed;
-    private float lookAhead;
-
-    private void Update()
+    // Update is called once per frame
+    void Update()
     {
-        //Room camera
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currentPosX, transform.position.y, transform.position.z), ref velocity, speed);
-
-        //Follow player
-        //transform.position = new Vector3(player.position.x + lookAhead, transform.position.y, transform.position.z);
-        //lookAhead = Mathf.Lerp(lookAhead, (aheadDistance * player.localScale.x), Time.deltaTime * cameraSpeed);
+        FollowTarget();
     }
 
-    public void MoveToNewRoom(Transform _newRoom)
+    private void FollowTarget()
     {
-        currentPosX = _newRoom.position.x;
+        if (target != null) // Periksa apakah target tidak null sebelum mengakses posisinya
+        {
+            Vector3 newPosition = new Vector2(target.position.x, target.position.y + 2);
+            transform.position = newPosition;
+        }
     }
 }
